0a1daf6077e7c37dcfc24352849a5267d79f6e0b
mizukiyoshino1 <mizukiyoshino32@gmail.com>
2022-10-01 22:17:57 +0800

    Initial commit

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+  <title>Book</title>
+  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="./reset.css">
+</head>
+
+<body>
+
+  <div class="top-page">
+    <header>
+      <ul class="nav">
+        <li class="list"><a href="#PURPOSE">PURPOSE</a></li>
+        <li class="list"><a href="#BUSINESS">BUSINESS</a></li>
+        <li class="list"><a href="#DESIGN">DESIGN</a></li>
+        <li class="list"><a href="#MOVIE">MOVIE</a></li>
+      </ul>
+    </header>
+    
+    <div class="main">
+      <h1 id="title">LET'S READ A BOOK</h1>
+    </div>
+  </div>
+
+  <div class="purpose">
+    <h1 id="PURPOSE">PURPOSE</h1>
+
+    <div class="img">
+        <img src="./img/goodsign.jpg" alt="ok" class="sign">   
+    </div>
+
+    <p class="content" id="top-content">僕は読書が大好きだ。もっと多くの人に本を読むようアドバイスしたい。<br class="sentence1">本の中には、まったく新しい世界が広がっているんだよ。<br>旅行に行く余裕がなくても、本を読めば心の中で旅することができる。<br>本の世界では、何でも見たいものをみて、どこでも行きたいところに行ける。</p>
+
+    <p class="content" id="buttom-content">
+      I love to read. I wish I could advise more people to read. There's a whole new world in books. If you can't afford to travel, you travel mentally through reading. You can see anything and go any place you want to in reading.
+    </p>
+
+    <p class="copy">マイケル・ジャクソン（米国のシンガーソングライター / 1958～2009） Wikipedia</p>
+  </div>
+
+  <section class="book1">
+    <h1 id="BUSINESS">BUSINESS</h1>
+
+    <div class="contents">
+      <div class="content-A">
+        <a href="https://www.amazon.co.jp/%EF%BC%91%E5%88%86%E3%81%A7%E8%A9%B1%E3%81%9B-%E4%B8%96%E7%95%8C%E3%81%AE%E3%83%88%E3%83%83%E3%83%97%E3%81%8C%E7%B5%B6%E8%B3%9B%E3%81%97%E3%81%9F%E5%A4%A7%E4%BA%8B%E3%81%AA%E3%81%93%E3%81%A8%E3%81%A0%E3%81%91%E3%82%B7%E3%83%B3%E3%83%97%E3%83%AB%E3%81%AB%E4%BC%9D%E3%81%88%E3%82%8B%E6%8A%80%E8%A1%93-%E4%BC%8A%E8%97%A4-%E7%BE%8A%E4%B8%80-ebook/dp/B07BFNPD6Q/ref=sr_1_1?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=2WCDCB5FES4VP&keywords=1%E5%88%86%E3%81%A7%E8%A9%B1%E3%81%9B&qid=1661063969&sprefix=1%E5%88%86%E3%81%A7%E8%A9%B1%E3%81%9B%2Caps%2C322&sr=8-1">
+          <img src="./img/book1.png" alt="book" class="img_wrap">
+        </a>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="like1" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.854 15.854A.5.5 0 0 1 2 15.5V14H.5a.5.5 0 0 1-.354-.854l1.5-1.5A.5.5 0 0 1 2 11.5h1.793l3.103-3.104a.5.5 0 1 1 .708.708L4.5 12.207V14a.5.5 0 0 1-.146.354l-1.5 1.5ZM16 3.5a.5.5 0 0 1-.854.354L14 2.707l-1.006 1.006c.236.248.44.531.6.845.562 1.096.585 2.517-.213 4.092-.793 1.563-2.395 3.288-5.105 5.08L8 13.912l-.276-.182A23.825 23.825 0 0 1 5.8 12.323L8.31 9.81a1.5 1.5 0 0 0-2.122-2.122L3.657 10.22a8.827 8.827 0 0 1-1.039-1.57c-.798-1.576-.775-2.997-.213-4.093C3.426 2.565 6.18 1.809 8 3.233c1.25-.98 2.944-.928 4.212-.152L13.292 2 12.147.854A.5.5 0 0 1 12.5 0h3a.5.5 0 0 1 .5.5v3Z"/>
+        </svg>
+      </div>
+      <div class="content-B">
+        <h2 id="bookname1">1分で話せ</h2>
+        <p id="abstract1">プレゼンに限らず、人前に立って話をする、指示をする、伝える、ということが苦手な方はいるでしょう。著者の伊藤氏は、そのプレゼンを聞いたソフトバンクの孫社長から認められるほどの技術の持ち主であり、今はグロービスの講師として、ヤフーアカデミアの学長として、起業家からビジネスパーソンまで年間300人以上のプレゼンを指導し、ピッチコンテストなどでの優勝者を続々と輩出しています。本書では、「右脳と左脳」に働きかける伊藤氏独特のメソッドを紹介します。
+        </p>
+      </div>
+    </div>
+
+    <div class="contents">
+      <div class="content-B" id="change-contents2">
+        <h2 id="bookname1">具体⇄抽象トレーニング<br><br>思考力が飛躍的にアップする29問</h2>
+        <!-- <h3>思考力が飛躍的にアップする29問</h3> -->
+        <p id="abstract1">「具体と抽象（の往復）」。その思考回路を持つと、あなたの知的能力は劇的に進化する！「具体⇔抽象」とは、抽象化と具体化という形で具体と抽象を行き来する思考法のこと。斬新な発想をできるようになるだけでなく、無用な軋轢やコミュニケーションギャップの解消にも役立ちます。そこで本書では、「抽象化と具体化の基本動作」から「仕事・日常生活における実践・応用の仕方」まで解説するとともに、トレーニング問題も多数用意しました。</p>
+      </div>
+      <div class="content-A" id="change-contents1">
+        <a href="https://www.amazon.co.jp/%E3%80%8C%E5%85%B7%E4%BD%93%E2%87%94%E6%8A%BD%E8%B1%A1%E3%80%8D%E3%83%88%E3%83%AC%E3%83%BC%E3%83%8B%E3%83%B3%E3%82%B0-%E6%80%9D%E8%80%83%E5%8A%9B%E3%81%8C%E9%A3%9B%E8%BA%8D%E7%9A%84%E3%81%AB%E3%82%A2%E3%83%83%E3%83%97%E3%81%99%E3%82%8B29%E5%95%8F-PHP%E3%83%93%E3%82%B8%E3%83%8D%E3%82%B9%E6%96%B0%E6%9B%B8-%E7%B4%B0%E8%B0%B7-%E5%8A%9F-ebook/dp/B0868GMSBG/ref=sr_1_2?crid=2RFAQ9YZTWW0Z&keywords=%E5%85%B7%E4%BD%93%E6%8A%BD%E8%B1%A1&qid=1661064053&sprefix=%E5%85%B7%E4%BD%93%E6%8A%BD%E8%B1%A1%2Caps%2C268&sr=8-2">
+          <img src="./img/book2.png" alt="book" class="img_wrap">
+        </a>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="like1" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.854 15.854A.5.5 0 0 1 2 15.5V14H.5a.5.5 0 0 1-.354-.854l1.5-1.5A.5.5 0 0 1 2 11.5h1.793l3.103-3.104a.5.5 0 1 1 .708.708L4.5 12.207V14a.5.5 0 0 1-.146.354l-1.5 1.5ZM16 3.5a.5.5 0 0 1-.854.354L14 2.707l-1.006 1.006c.236.248.44.531.6.845.562 1.096.585 2.517-.213 4.092-.793 1.563-2.395 3.288-5.105 5.08L8 13.912l-.276-.182A23.825 23.825 0 0 1 5.8 12.323L8.31 9.81a1.5 1.5 0 0 0-2.122-2.122L3.657 10.22a8.827 8.827 0 0 1-1.039-1.57c-.798-1.576-.775-2.997-.213-4.093C3.426 2.565 6.18 1.809 8 3.233c1.25-.98 2.944-.928 4.212-.152L13.292 2 12.147.854A.5.5 0 0 1 12.5 0h3a.5.5 0 0 1 .5.5v3Z"/>
+        </svg>
+      </div>
+    </div>
+  </section>
+
+  <section class="book2">
+    <h1 id="DESIGN">DESIGN</h1>
+
+    <div class="contents">
+      <div class="content-A">
+        <a href="https://www.amazon.co.jp/%EF%BC%A8%EF%BC%A5%EF%BC%AC%EF%BC%AC%EF%BC%AF%EF%BC%8C%EF%BC%A4%EF%BC%A5%EF%BC%B3%EF%BC%A9%EF%BC%A7%EF%BC%AE-%E6%97%A5%E6%9C%AC%E4%BA%BA%E3%81%A8%E3%83%87%E3%82%B6%E3%82%A4%E3%83%B3-NewsPicks-Book-%E7%9F%B3%E5%B7%9D%E4%BF%8A%E7%A5%90-ebook/dp/B07P45QNMN/ref=sr_1_1?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=29BU27Q6DORF5&keywords=HELLO+DESIGN&qid=1661064128&sprefix=hello+design%2Caps%2C265&sr=8-1">
+          <img src="./img/book3.png" alt="book" class="img_wrap">
+        </a>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="like1" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.854 15.854A.5.5 0 0 1 2 15.5V14H.5a.5.5 0 0 1-.354-.854l1.5-1.5A.5.5 0 0 1 2 11.5h1.793l3.103-3.104a.5.5 0 1 1 .708.708L4.5 12.207V14a.5.5 0 0 1-.146.354l-1.5 1.5ZM16 3.5a.5.5 0 0 1-.854.354L14 2.707l-1.006 1.006c.236.248.44.531.6.845.562 1.096.585 2.517-.213 4.092-.793 1.563-2.395 3.288-5.105 5.08L8 13.912l-.276-.182A23.825 23.825 0 0 1 5.8 12.323L8.31 9.81a1.5 1.5 0 0 0-2.122-2.122L3.657 10.22a8.827 8.827 0 0 1-1.039-1.57c-.798-1.576-.775-2.997-.213-4.093C3.426 2.565 6.18 1.809 8 3.233c1.25-.98 2.944-.928 4.212-.152L13.292 2 12.147.854A.5.5 0 0 1 12.5 0h3a.5.5 0 0 1 .5.5v3Z"/>
+        </svg>
+      </div>
+      <div class="content-B">
+        <h2 id="bookname1">HELLO DESIGN</h2>
+        <h3>日本人とデザイン</h3>
+        <p id="abstract1">デザインとは本来、「人間の欲求に寄り添い、解決する、誰にでもできる考え方」。
+          トップデザイナーのように「誰も思い浮かばなかった優れた答えを導き出す」思考のメソッドを、イノベーティブ企業として有名なデザインコンサルティングファーム『IDEO』に所属していた著者が伝授。日本人が今、身に着けるべき理由から、その考え方、活用術まで。
+          考え方も、発想の質も、生き方もガラッと変わる「デザイン思考」の決定版。</p>
+      </div>
+    </div>
+
+    <div class="contents">
+      <div class="content-B" id="change-contents4">
+        <h2 id="bookname1">スペキュラティヴデザイン</h2>
+        <h3>問題解決から問題提起へ</h3>
+        <p id="abstract1">企業のための問題解決、商品の売上向上。デザインと聞くとこうしたことを思い浮かべる人がほとんでしょう。しかし、現代の私たちが直面する課題の多くはもはや解決不能で、これらを克服するためには、私たちの価値観、信念、考え方を変えるしか他に手はありません。「問い」を生み出し、未来のシナリオをデザインすることで、今ある世界に別の可能性を提示する「スペキュラティヴ・デザイン」について紹介した初の日本語版書籍です。ロイヤル・カレッジ・オブ・アート(RCA)のデザイン・インタラクティブ学科で10年にわたって教鞭を執り、「スペキュラティヴ・デザイン」の提唱者として世界的に注目を集めている著者が、アート・小説・イラスト・写真・映画などあらゆる領域を引証しながら、未来をスペキュレート〈思索〉する視点について紹介します。</p>
+      </div>
+      <div class="content-A" id="change-contents3">
+        <a href="https://www.amazon.co.jp/%E3%82%B9%E3%83%9A%E3%82%AD%E3%83%A5%E3%83%A9%E3%83%86%E3%82%A3%E3%83%B4%E3%83%BB%E3%83%87%E3%82%B6%E3%82%A4%E3%83%B3-%E5%95%8F%E9%A1%8C%E8%A7%A3%E6%B1%BA%E3%81%8B%E3%82%89%E3%80%81%E5%95%8F%E9%A1%8C%E6%8F%90%E8%B5%B7%E3%81%B8%E3%80%82%E2%80%94%E6%9C%AA%E6%9D%A5%E3%82%92%E6%80%9D%E7%B4%A2%E3%81%99%E3%82%8B%E3%81%9F%E3%82%81%E3%81%AB%E3%83%87%E3%82%B6%E3%82%A4%E3%83%B3%E3%81%8C%E3%81%A7%E3%81%8D%E3%82%8B%E3%81%93%E3%81%A8-%E3%82%A2%E3%83%B3%E3%82%BD%E3%83%8B%E3%83%BC%E3%83%BB%E3%83%80%E3%83%B3/dp/4802510020/ref=sr_1_1?crid=13CQ0UZHQ4592&keywords=%E3%82%B9%E3%83%9A%E3%82%AD%E3%83%A5%E3%83%A9%E3%83%86%E3%82%A3%E3%83%96%E3%83%87%E3%82%B6%E3%82%A4%E3%83%B3&qid=1661064157&sprefix=%E3%81%99%E3%81%BA%E3%81%8D%E3%82%85%E3%82%89%2Caps%2C319&sr=8-1">
+          <img src="./img/book4.png" alt="book" class="img_wrap">
+        </a>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="like1" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.854 15.854A.5.5 0 0 1 2 15.5V14H.5a.5.5 0 0 1-.354-.854l1.5-1.5A.5.5 0 0 1 2 11.5h1.793l3.103-3.104a.5.5 0 1 1 .708.708L4.5 12.207V14a.5.5 0 0 1-.146.354l-1.5 1.5ZM16 3.5a.5.5 0 0 1-.854.354L14 2.707l-1.006 1.006c.236.248.44.531.6.845.562 1.096.585 2.517-.213 4.092-.793 1.563-2.395 3.288-5.105 5.08L8 13.912l-.276-.182A23.825 23.825 0 0 1 5.8 12.323L8.31 9.81a1.5 1.5 0 0 0-2.122-2.122L3.657 10.22a8.827 8.827 0 0 1-1.039-1.57c-.798-1.576-.775-2.997-.213-4.093C3.426 2.565 6.18 1.809 8 3.233c1.25-.98 2.944-.928 4.212-.152L13.292 2 12.147.854A.5.5 0 0 1 12.5 0h3a.5.5 0 0 1 .5.5v3Z"/>
+        </svg>
+      </div>
+    </div>
+
+  </section>
+
+  <section class="youtube">
+    <h1 id="MOVIE">MOVIE</h1>
+
+    <div class="contents">
+      
+      <div class="content-A">
+        <iframe class="movie1"  src="https://www.youtube.com/embed/KoukFuP7eO0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="like2" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.854 15.854A.5.5 0 0 1 2 15.5V14H.5a.5.5 0 0 1-.354-.854l1.5-1.5A.5.5 0 0 1 2 11.5h1.793l3.103-3.104a.5.5 0 1 1 .708.708L4.5 12.207V14a.5.5 0 0 1-.146.354l-1.5 1.5ZM16 3.5a.5.5 0 0 1-.854.354L14 2.707l-1.006 1.006c.236.248.44.531.6.845.562 1.096.585 2.517-.213 4.092-.793 1.563-2.395 3.288-5.105 5.08L8 13.912l-.276-.182A23.825 23.825 0 0 1 5.8 12.323L8.31 9.81a1.5 1.5 0 0 0-2.122-2.122L3.657 10.22a8.827 8.827 0 0 1-1.039-1.57c-.798-1.576-.775-2.997-.213-4.093C3.426 2.565 6.18 1.809 8 3.233c1.25-.98 2.944-.928 4.212-.152L13.292 2 12.147.854A.5.5 0 0 1 12.5 0h3a.5.5 0 0 1 .5.5v3Z"/>
+        </svg>
+      </div>
+    
+      <div class="content-A">
+        <iframe class="movie1"  src="https://www.youtube.com/embed/BZmAPHXh3MQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="like2" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.854 15.854A.5.5 0 0 1 2 15.5V14H.5a.5.5 0 0 1-.354-.854l1.5-1.5A.5.5 0 0 1 2 11.5h1.793l3.103-3.104a.5.5 0 1 1 .708.708L4.5 12.207V14a.5.5 0 0 1-.146.354l-1.5 1.5ZM16 3.5a.5.5 0 0 1-.854.354L14 2.707l-1.006 1.006c.236.248.44.531.6.845.562 1.096.585 2.517-.213 4.092-.793 1.563-2.395 3.288-5.105 5.08L8 13.912l-.276-.182A23.825 23.825 0 0 1 5.8 12.323L8.31 9.81a1.5 1.5 0 0 0-2.122-2.122L3.657 10.22a8.827 8.827 0 0 1-1.039-1.57c-.798-1.576-.775-2.997-.213-4.093C3.426 2.565 6.18 1.809 8 3.233c1.25-.98 2.944-.928 4.212-.152L13.292 2 12.147.854A.5.5 0 0 1 12.5 0h3a.5.5 0 0 1 .5.5v3Z"/>
+        </svg>
+      </div>
+
+    </div>
+
+  </section>
+
+  <footer>
+    <div class="bottom">
+      <small>Copyright &copy; 吉野瑞己</small>
+    
+      <a href="https://www.instagram.com/mizukiyoshin.o/">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="instagram" viewBox="0 0 16 16">
+          <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+        </svg>
+      </a>
+
+      <a href="https://twitter.com/mizukiiyoshino">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="twitter" viewBox="0 0 16 16">
+          <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+        </svg>
+      </a>
+
+      <a href="https://www.facebook.com/profile.php?id=100071668584687">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="facebook" viewBox="0 0 16 16">
+          <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+        </svg>
+      </a>
+
+    </div>
+
+  </footer>
+
+  <a href="#" class="gotop">
+    <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+      <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
+    </svg>
+  </a>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="./index.js"></script>
+  
+</body>
+</html>
